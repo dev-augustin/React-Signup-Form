f4f4f51454604e332b7b293a59c95f3a6a9ad463
dev-augustin <dev.augustin00@gmail.com>
2020-04-19 09:10:03 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import Form from "./components/Form";
-import UserInfo from "./components/UserInfo";
+//import UserInfo from "./components/UserInfo.js";
 
-function App() {
-  return (
-    <div className="App">
-    <Form/>
-    <UserInfo/>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+   
+    return (
+      <div className="App">
+      <h1>SignUp Form</h1>
+      <div><Form/></div>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
